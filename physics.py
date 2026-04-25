eaaf50dc5d5ba5bd748d08a6f9dcebd733da8e09
Jotham Duwe <jotham42@outlook.com>
2026-04-25 17:38:32 +0300

    Add functions for lift, drag, and weight calculations

--- physics.py
+++ physics.py
@@ -1,1 +1,13 @@
+import math
 
+rho = 1.225
+g = 9.81
+
+def lift(v, S, Cl):
+    return 0.5 * rho * v**2 * S * Cl
+
+def drag(v, S, Cd):
+    return 0.5 * rho * v**2 * S * Cd
+
+def weight(m):
+    return m * g
